1c66e05d4292f16a07e64053e5abb3f46b7b3afb
bhavesh <bhavesh.bece.13@gmail.com>
2018-08-11 00:21:45 +0530

    Home, Course, Project Menu click and shows the Fragment based on that menu click.

--- app/src/main/java/vsl/bhavesh/fragmentdemo/MainActivity.kt
+++ app/src/main/java/vsl/bhavesh/fragmentdemo/MainActivity.kt
@@ -2,11 +2,43 @@ package vsl.bhavesh.fragmentdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+
+        var fManager = supportFragmentManager // get support fragment manageger
+
+        // When click on Project Tab
+        projects.setOnClickListener {
+
+            var tx = fManager.beginTransaction()  // get fragmenet transation class object
+            tx.replace(R.id.fragment1,ProjectFragment())
+            tx.commit()
+        }
+
+
+        // Call Home fragment
+        home.setOnClickListener {
+
+            var tx = fManager.beginTransaction()  // get fragmenet transation class object
+            tx.replace(R.id.fragment1,HomeFragment())
+            tx.commit()
+        }
+
+
+        // Call Course fragment
+        courses.setOnClickListener {
+            var tx = fManager.beginTransaction()  // get fragmenet transation class object
+            tx.replace(R.id.fragment1,CourseFragment())
+            tx.commit()
+
+        }
+
+    }// On Create
+
 }
---
+++ app/src/main/java/vsl/bhavesh/fragmentdemo/ProjectFragment.kt
@@ -0,0 +1,18 @@
+package vsl.bhavesh.fragmentdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProjectFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var v = inflater.inflate(R.layout.project, container, false)
+        return v
+        //return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
+}
